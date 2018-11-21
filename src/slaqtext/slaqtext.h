b8790f53cf95402824bb13e79d56f431f7123caa
Vladyslav Stelmakhovskyi <vladyslav.stelmakhovski@volvocars.com>
2018-11-21 14:57:45 +0100

    Added missed files

---
+++ src/slaqtext/slaqtext.h
@@ -0,0 +1,146 @@
+#pragma once
+
+#include <QtQuickTemplates2/private/qquicklabel_p.h>
+#include <QtQuickControls2/private/qtquickcontrols2global_p.h>
+
+class SlackTextPrivate;
+
+class SlackText : public QQuickLabel
+{
+
+    Q_OBJECT
+
+    Q_PROPERTY(QString text READ text WRITE setText NOTIFY textChanged)
+    Q_PROPERTY(TextFormat textFormat READ textFormat WRITE setTextFormat NOTIFY textFormatChanged)
+
+    Q_PROPERTY(QColor selectionColor READ selectionColor WRITE setSelectionColor NOTIFY selectionColorChanged)
+    Q_PROPERTY(QColor selectedTextColor READ selectedTextColor WRITE setSelectedTextColor NOTIFY selectedTextColorChanged)
+    Q_PROPERTY(int selectionStart READ selectionStart NOTIFY selectionStartChanged)
+    Q_PROPERTY(int selectionEnd READ selectionEnd NOTIFY selectionEndChanged)
+    Q_PROPERTY(QString selectedText READ selectedText NOTIFY selectedTextChanged)
+    Q_PROPERTY(bool selectByMouse READ selectByMouse WRITE setSelectByMouse NOTIFY selectByMouseChanged)
+    Q_PROPERTY(SelectionMode mouseSelectionMode READ mouseSelectionMode WRITE setMouseSelectionMode NOTIFY mouseSelectionModeChanged)
+    Q_PROPERTY(bool persistentSelection READ persistentSelection WRITE setPersistentSelection NOTIFY persistentSelectionChanged)
+
+    Q_PROPERTY(qreal padding READ padding WRITE setPadding RESET resetPadding NOTIFY paddingChanged)
+    Q_PROPERTY(qreal topPadding READ topPadding WRITE setTopPadding RESET resetTopPadding NOTIFY topPaddingChanged)
+    Q_PROPERTY(qreal leftPadding READ leftPadding WRITE setLeftPadding RESET resetLeftPadding NOTIFY leftPaddingChanged)
+    Q_PROPERTY(qreal rightPadding READ rightPadding WRITE setRightPadding RESET resetRightPadding NOTIFY rightPaddingChanged)
+    Q_PROPERTY(qreal bottomPadding READ bottomPadding WRITE setBottomPadding RESET resetBottomPadding NOTIFY bottomPaddingChanged)
+
+    Q_PROPERTY(QString hoveredLink READ hoveredLink NOTIFY linkHovered)
+    Q_PROPERTY(QString hoveredImage READ hoveredImage NOTIFY imageHovered)
+
+    Q_PROPERTY(QJSValue fontInfo READ fontInfo NOTIFY fontInfoChanged)
+    Q_PROPERTY(QSizeF advance READ advance NOTIFY contentSizeChanged)
+
+public:
+    enum SelectionMode {
+        SelectCharacters,
+        SelectWords
+    };
+    Q_ENUM(SelectionMode)
+
+    explicit SlackText(QQuickItem *parent = nullptr);
+
+    virtual ~SlackText() = default;
+
+    //Auxilliary functions needed to control the TextInput from QML
+    Q_INVOKABLE void positionAt(QQmlV4Function *args);
+    Q_INVOKABLE QRectF positionToRectangle(int pos);
+    Q_INVOKABLE void moveCursorSelection(int pos);
+    Q_INVOKABLE void moveCursorSelection(int pos, SelectionMode mode);
+
+    QColor selectionColor() const;
+    QColor selectedTextColor() const;
+    int selectionStart() const;
+    int selectionEnd() const;
+    QString selectedText() const;
+    bool selectByMouse() const;
+    SelectionMode mouseSelectionMode() const;
+    bool persistentSelection() const;
+
+    QString text() const;
+    void setText(const QString &txt);
+
+    TextFormat textFormat() const;
+    void setTextFormat(TextFormat format);
+
+//    qreal topPadding() const;
+//    void setTopPadding(qreal padding);
+//    void resetTopPadding();
+
+    QString hoveredLink() const;
+    QString hoveredImage() const;
+
+Q_SIGNALS:
+    //void topPaddingChanged();
+    void textChanged(const QString &text);
+    void textFormatChanged(QQuickText::TextFormat textFormat);
+    void selectionColorChanged();
+    void selectedTextColorChanged();
+    void selectionStartChanged();
+    void selectionEndChanged();
+    void selectedTextChanged();
+    void selectByMouseChanged(bool selectByMouse);
+    void mouseSelectionModeChanged(SelectionMode mouseSelectionMode);
+    void persistentSelectionChanged();
+
+    void linkHovered(const QString &link);
+    void imageHovered(const QString &imagelink, qreal x, qreal y);
+
+public Q_SLOTS:
+    void copy();
+    void selectAll();
+    void selectWord();
+    void select(int start, int end);
+    void deselect();
+    void setSelectionColor(const QColor &color);
+    void setSelectedTextColor(const QColor &color);
+    void setSelectByMouse(bool on);
+    void setMouseSelectionMode(SelectionMode mouseSelectionMode);
+    void setPersistentSelection(bool persistentSelection);
+
+
+private Q_SLOTS:
+    void selectionChanged();
+    void prepareText();
+    void onImageLoaded(const QString &id);
+
+protected:
+    void componentComplete() override;
+    //SlackText(SlackTextPrivate &dd, QQuickItem *parent = nullptr);
+
+    void mousePressEvent(QMouseEvent *event) override;
+    void mouseMoveEvent(QMouseEvent *event) override;
+    void mouseReleaseEvent(QMouseEvent *event) override;
+    void mouseDoubleClickEvent(QMouseEvent *event) override;
+    void mouseUngrabEvent() override;
+    QSGNode *updatePaintNode(QSGNode *oldNode, UpdatePaintNodeData *data) override;
+    //void updatePolish() override;
+
+    void hoverEnterEvent(QHoverEvent *event) override;
+    void hoverMoveEvent(QHoverEvent *event) override;
+    void hoverLeaveEvent(QHoverEvent *event) override;
+
+
+private:
+    void invalidateFontCaches();
+    void insertImage(QTextCursor &cursor, const QString &url, const QImage &img);
+
+    QColor m_selectionColor;
+    QColor m_selectedTextColor;
+    int m_selectionStart;
+    int m_selectionEnd;
+    QString m_selectedText;
+    bool m_selectByMouse;
+    SelectionMode m_mouseSelectionMode;
+    bool m_persistentSelection;
+
+private:
+    SlackTextPrivate* d_ptr { nullptr };
+    Q_DECLARE_PRIVATE(SlackText)
+    Q_DISABLE_COPY(SlackText)
+};
+
+QML_DECLARE_TYPE(SlackText)
